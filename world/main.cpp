d2f4011bbb881b309014dec362300d1305e88aa4
Dmitry Khovyakov <dmitry.khovyakov@dev-pro.net>
2016-12-01 13:30:26 +0200

    Build script and entry point for application

---
+++ world/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by dmitry.khovyakov on 11/25/2016.
+//
+
+#include "../meng/Application.h"
+#include "WorldState.h"
+#include "../meng/view/Screen.h"
+
+int main(int argc, char* argv[]) {
+    MEng::Application& app = MEng::Application::getInstance();
+
+    MEng::View::Screen::getInstance().init(800, 600);
+
+    app.pushState(std::make_shared<WorldState>());
+    app.run();
+}
